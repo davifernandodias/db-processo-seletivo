cb85c1442b680599cf3f4c099e1eb429a5594e22
davi dias <davifernandosousadias18@gmail.com>
2024-11-08 09:20:35 -0300

    refactor: update

--- api/server.js
+++ api/server.js
@@ -1,31 +1,24 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const cors = require('cors'); // Importando o middleware CORS
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+// Permitir requisições de qualquer origem ou de um domínio específico
+server.use(cors({
+  origin: 'https://to-do-list-task-processo-seletivo-cod511qja.vercel.app', // Adicione o URL do seu front-end
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Os métodos permitidos
+  allowedHeaders: ['Content-Type'], // Cabeçalhos permitidos
+}));
 
-// Comment out to allow write operations
 const router = jsonServer.router('db.json')
-
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+  console.log('JSON Server is running')
 })
-
-// Export the Server API
-module.exports = server
